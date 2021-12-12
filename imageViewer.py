914fa6c32f8abe3b7af66207ae9de07bd8662f19
parsahonarmand <parsa.honarmand@gmail.com>
2021-12-12 16:02:35 -0700

    Imageviewer code added

--- imageViewer.py
+++ imageViewer.py
@@ -13,6 +13,8 @@ import numpy as np
 import idx2numpy
 import matplotlib.pyplot as plt
 import sys
+from network import loadFromFile
+from MNIST_starter import *
 
 imagefile = "data/t10k-images.idx3-ubyte"    # can change these to read training data
 datafile = "data/t10k-labels.idx1-ubyte"
@@ -34,9 +36,22 @@ except:
 # TO DO: get label from other file
 label = dataarray[index]
 
+# To run this, comment out the lines 84-88 in MNIST_starter.py
+# To avoid training the net
+mnist = loadFromFile("part1.pkl")
+training, testing = prepData()
+test_results = [(np.argmax(mnist.feedforward(x)), y) for (x, y) in testing]
+sum(int(x == y) for (x, y) in test_results)
+counter = 0
+for idx, (x,y) in enumerate(test_results):
+    if (int(x != y)) and counter <= 2:
+        print("result from training: " + str(x))
+        print("correct label: " + str(y))
+        print("at index: " + str(idx))
+        plt.imshow(imagearray[idx], cmap=plt.cm.binary)
+        plt.show()
+        counter += 1
 
-print("Displaying test image")
-print(f"index: {index}, label: {label}")
 
-plt.imshow(imagearray[index], cmap=plt.cm.binary)
-plt.show()
+# print("Displaying test image")
+# print(f"index: {index}, label: {label}")
